345116250967c473f13bc7bace1b250e49d0f678
Jack Fawthorpe <jfa93@uclive.ac.nz>
2023-05-23 11:06:55 +1200

    Feature: Removed enter from search bar and made autoquery work with an empty query string

--- film-festival/src/components/Films/FilmSearchCard.tsx
+++ film-festival/src/components/Films/FilmSearchCard.tsx
@@ -6,26 +6,18 @@ type FilmNavProps = {
     changeFilmQuery: (action: SetStateAction<FilmSearchQuery>) => void;
 }
 
-const FilmSearchCard = (props: FilmNavProps) => {
-
-    const [input, setInput] = useState('');
-
-    const handleKeyPress = (event: any) => {
-        if (event.key === 'Enter') {
-            handleSubmit();
-        }
-    };
-
+const FilmSearchCard = ({changeFilmQuery}: FilmNavProps) => {
     const handleInputChange = (event: any) => {
-        setInput(event.target.value);
-    }
-
-    const handleSubmit = () => {
-        if (input != "") {
-            props.changeFilmQuery((prev: FilmSearchQuery) => ({
+        if (event.target.value != "") {
+            changeFilmQuery((prev: FilmSearchQuery) => ({
                 ...prev,
-                q: input
+                q: event.target.value
             }))
+        } else {
+            changeFilmQuery((prev: FilmSearchQuery) => {
+                const {q, ...rest} = prev;
+                return rest
+            })
         }
     }
 
@@ -36,10 +28,7 @@ const FilmSearchCard = (props: FilmNavProps) => {
             </div>
             <div className='p-2 d-flex'>
                 <div className='flex-grow-1'>
-                    <input className={`form-control`} onInput={handleInputChange} onKeyDown={handleKeyPress}/>
-                </div>
-                <div className='ps-2'>
-                    <button className={`btn btn-primary`} onClick={handleSubmit}>Search</button>
+                    <input className={`form-control`} onInput={handleInputChange} placeholder={"Enter a movie title or description"}/>
                 </div>
             </div>
         </div>
