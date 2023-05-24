60b304bbbaa42caca94b64fc33b4d95fcfc5fa37
Jack Fawthorpe <jfa93@uclive.ac.nz>
2023-05-24 13:59:28 +1200

    Feature: Doesn't squash reviewers images anymore

--- film-festival/src/components/Film/Review.tsx
+++ film-festival/src/components/Film/Review.tsx
@@ -12,18 +12,18 @@ const Review = (review: Review) => {
     return (
         <>
             <div className='col border-bottom pt-2 container-fluid'>
-                <div className='row py-2'>
-                    <div className='col-1'>
+                <div className='row py-2 d-flex'>
+                    <div className='col-auto'>
                         <img src={getBaseUrl() + "/users/" + review.reviewerId + "/image?" + Date.now()}
                              className="rounded float-end img-thumbnail director-image text-center"
                              alt="Director's Picture"
                              onError={replaceImage}/>
                     </div>
-                    <div className='col-2 pt-2 border-end'>
+                    <div className='col-md-3 col-sm-6 pt-2 border-end'>
                         <h5>{review.reviewerFirstName} {review.reviewerLastName}</h5>
                         <h6>{review.rating}/10 ⭐</h6>
                     </div>
-                    <div className='col-8'>{review.review}</div>
+                    <div className='col'>{review.review}</div>
                 </div>
             </div>
         </>
